aba163b9ad39e9a4db13e0e854bffb16bf9fbb60
dochin26 <yuuta.2635@gmail.com>
2025-09-04 19:03:26 +0900

    fix:credentialにpublic URLを追加

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,14 @@
 module PostsHelper
+  # post.body_image の URL を環境に応じて返す
+  def image_url_for(image)
+    return unless image.attached?
+
+    if Rails.env.production? && Rails.application.credentials.r2[:public_url].present?
+      # 本番環境では R2 の URL を返す
+      "#{Rails.application.credentials.r2[:public_url]}/#{image.key}"
+    else
+      # 開発環境や R2 URL がない場合は ActiveStorage の url_for を使用
+      url_for(image)
+    end
+  end
 end
