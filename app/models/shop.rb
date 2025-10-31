aa46c21912c9dcc0c9d34530d98ef5478c5bacff
dochin26 <yuuta.2635@gmail.com>
2025-11-01 02:51:04 +0900

    add:酒・食べ物の複数登録機能の実装
    タグ機能の実装。当初Sake_tags・Food_tags、Sake_maps・Food_mapsの中間テーブル含め4テーブルを新規作成予定でしたが、既存テーブルで実現可能なため変更。酒、料理の区切りはスペースでフレーズを判定しています。

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -8,42 +8,47 @@ class Shop < ApplicationRecord
     has_many :foods, through: :shop_foods
     has_many :shop_places, dependent: :destroy
 
-    accepts_nested_attributes_for :shop_sakes, allow_destroy: true
-    accepts_nested_attributes_for :shop_foods, allow_destroy: true
-    accepts_nested_attributes_for :sakes
-    accepts_nested_attributes_for :foods
     accepts_nested_attributes_for :shop_places
 
-    before_validation :find_or_assign_existing_sakes
-    before_validation :find_or_assign_existing_foods
+    attr_accessor :sake_names_input, :food_names_input
 
     def self.ransackable_attributes(auth_object = nil)
         %w[id name introduction created_at updated_at]
     end
 
     def self.ransackable_associations(auth_object = nil)
-        %w[posts sakes foods]
+        %w[posts sakes foods shop_places]
     end
 
-    private
+    # スペース区切りの文字列から酒のタグを更新
+    def update_sake_tags(sake_names_string)
+        return if sake_names_string.nil?
 
-    def find_or_assign_existing_sakes
-    self.sakes = sakes.map do |sake|
-        if sake.name.present?
-        Sake.find_by(name: sake.name) || sake
-        else
-        sake
+        # 全角・半角スペースで分割
+        sake_names = sake_names_string.split(/[[:space:]]+/).reject(&:blank?)
+
+        # 新しいsakeのIDリストを作成
+        new_sake_ids = sake_names.map do |name|
+            Sake.find_or_create_by!(name: name).id
         end
-    end
+
+        # 既存の関連を完全に置き換え
+        self.sake_ids = new_sake_ids
     end
 
-    def find_or_assign_existing_foods
-    self.foods = foods.map do |food|
-        if food.name.present?
-        Food.find_by(name: food.name) || food
-        else
-        food
+    # スペース区切りの文字列から料理のタグを更新
+    def update_food_tags(food_names_string)
+        return if food_names_string.nil?
+
+        # 全角・半角スペースで分割
+        food_names = food_names_string.split(/[[:space:]]+/).reject(&:blank?)
+
+        # 新しいfoodのIDリストを作成
+        new_food_ids = food_names.map do |name|
+            Food.find_or_create_by!(name: name).id
         end
-    end
+
+        # 既存の関連を完全に置き換え
+        self.food_ids = new_food_ids
     end
 end
