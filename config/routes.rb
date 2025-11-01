f4fc0f9d7ba8e7a92868f004d5d9c14488180ce3
dochin26 <yuuta.2635@gmail.com>
2025-11-02 01:15:09 +0900

    add:マイページ機能の実装
    マイページ機能の内訳\n* いいね一覧\n* 投稿一覧

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
   }
   root "static_pages#index"
+  get "mypage", to: "my_pages#show"
   resources :posts, only: %i[index new show edit create update destroy]
-  resources :shops, only: [ :index, :show, :edit, :create, :update, :destroy ] do
-    resources :likes, only: [ :create, :destroy ]
+  resources :shops, only: %i[index show edit create update destroy] do
+    resources :likes, only: %i[create destroy]
   end
   get "location", to: "locations#show"
 end
