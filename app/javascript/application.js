d81b91dccfe947beb0391085895981cceef04944
dochin26 <yuuta.2635@gmail.com>
2025-11-03 23:10:00 +0900

    add:stimulus-autocompleteの追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,9 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
+import { Application } from '@hotwired/stimulus'
+import { Autocomplete } from 'stimulus-autocomplete'
 import "./controllers"
 import "./map"
+
+const application = Application.start()
+application.register('autocomplete', Autocomplete)
