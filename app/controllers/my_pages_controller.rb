e423b47a567eb48979fa15f7b9f2e86ed5d24122
dochin26 <yuuta.2635@gmail.com>
2025-11-18 00:45:51 +0900

    fix: マイページの歯車アイコンがずれてしまう
    ### 変更点* 歯車は廃止し区画ごとリンクボタンへ改修

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -4,6 +4,6 @@ class MyPagesController < ApplicationController
   def show
     @user = current_user
     @posts = @user.posts.includes(:shop, body_image_attachment: :blob).order(created_at: :desc).page(params[:posts_page])
-    @likes = @user.likes.includes(shop: [ :shop_places, :sakes, :foods ]).order(created_at: :desc).page(params[:likes_page])
+    @likes = @user.likes.includes(shop: [ :shop_places, :sakes, :foods, posts: { body_image_attachment: :blob } ]).order(created_at: :desc).page(params[:likes_page])
   end
 end
