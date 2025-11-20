ffa62519f93e02b4bc8d7ad85cbcba55520114ea
dochin26 <yuuta.2635@gmail.com>
2025-11-20 16:41:12 +0900

    fix: google mapの表示・検索の問題を修正
    ###修正内容* showまたはeditから投稿モーダルを表示すると、モーダル側のmapが表示しない問題* 緯度・経度を必須に変更。伴いRSpecも修正* footerの登録→投稿する、_modal_formの新規投稿→投稿する

--- spec/factories/shop_places.rb
+++ spec/factories/shop_places.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :shop_place do
     association :shop
-    latitude { nil }
-    longitude { nil }
+    latitude { 35.6812362 }
+    longitude { 139.7671248 }
     address { "東京都西東京市" }
   end
 end
